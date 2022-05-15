eba790936bfa9887f93ac1f758b70962bbb1352e
edward aung <neolaw@gmail.com>
2022-05-15 16:34:21 +1000

    end of day.

--- week07/utils.py
+++ week07/utils.py
@@ -1,4 +1,10 @@
 from io import StringIO
+from matplotlib.pyplot import gray
+import requests
+
+import cv2
+import numpy as np
+from scipy import misc
 import pandas as pd
 
 my_data = """id, length, width
@@ -13,3 +19,22 @@ readable_data = StringIO(my_data)
 
 df = pd.read_csv(readable_data, skiprows=2,names=[str(i) for i in range(3)])
 np_data = df.values
+
+def download_and_decode_cv2(url, grayscale=True):
+    rr = requests.get(url)
+    _nparr = np.frombuffer(rr.content, np.uint8)
+    _img = cv2.imdecode(_nparr, cv2.IMREAD_COLOR)
+    if grayscale:
+        _img = cv2.cvtColor(_img, cv2.COLOR_RGB2GRAY)
+    return _img
+
+def get_image(url=None, grayscale=True):
+    if url:
+        img = download_and_decode_cv2(url=url, grayscale=grayscale)
+        img = img/255
+    else:
+        img = misc.face()
+        img = img/255
+        if grayscale:
+            img = img @ [0.2126, 0.7152, 0.0722]
+    return img
